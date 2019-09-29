3b0c6fb692e476f91d4847aefcca06f562b7ef5b
Jack Treado <john.treado@yale.edu>
2019-09-28 20:23:14 -0400

    added module load command to gel process script

--- bash/gel/gel2D_postprocess.sh
+++ bash/gel/gel2D_postprocess.sh
@@ -117,6 +117,7 @@ echo \#SBATCH -n 1 >> $slurmf
 echo \#SBATCH -p $partition >> $slurmf
 echo \#SBATCH -J $job_name >> $slurmf
 echo \#SBATCH -o $runout >> $slurmf
+echo module load MATLAB >> $slurmf
 echo sed -n \"\$\{SLURM_ARRAY_TASK_ID\}p\" "$taskf" \| /bin/bash >> $slurmf
 cat $slurmf
 
