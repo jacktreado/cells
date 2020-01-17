92908df61fc4160feb6d23597955e736c18877b8
Jack Treado <john.treado@yale.edu>
2020-01-17 09:25:34 -0500

    relaxed Ptol and Ktol for cell jamming

--- main/cellJamming.cpp
+++ main/cellJamming.cpp
@@ -34,8 +34,8 @@ const double aInitial 		= 0.0;			// attraction parameter to start
 const double del 			= 1.0;			// radius of vertices in units of l0
 
 // tolerances
-const double Ktol 			= 1e-20;		// kinetic energy tolerance
-const double Ptol 			= 1e-8;			// pressure tolerance
+const double Ktol 			= 1e-16;		// kinetic energy tolerance
+const double Ptol 			= 1e-6;			// pressure tolerance
 
 // int main
 int main(int argc, char const *argv[])
