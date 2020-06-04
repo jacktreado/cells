17a73a4c184fa67e35adb8bbe72bf9deaf8c7d1d
Jack Treado <john.treado@yale.edu>
2020-06-04 15:40:42 -0400

    Decreased Ftol by 1 order of magnitude, so relaxed state either undercompressed or overcompressed

--- main/jamming/bidCellJamming.cpp
+++ main/jamming/bidCellJamming.cpp
@@ -33,7 +33,7 @@ const double a 				= 0.0;			// attraction parameter
 const double del 			= 1.0;			// radius of vertices in units of l0
 
 // tolerances
-const double Ftol 			= 1e-9;			// force tolerance (for FIRE min)
+const double Ftol 			= 1e-10;			// force tolerance (for FIRE min)
 const double Ptol 			= 1e-8;			// pressure tolerance
 
 // int main
