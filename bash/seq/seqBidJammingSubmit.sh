dd5b956b54355bc31291041c76c0505ee7075338
Jack Treado <john.treado@yale.edu>
2020-10-21 10:12:10 -0400

    removed requeue from seq bid jamming

--- bash/seq/seqBidJammingSubmit.sh
+++ bash/seq/seqBidJammingSubmit.sh
@@ -134,10 +134,6 @@ echo \#SBATCH -n 1 >> $slurmf
 echo \#SBATCH -p $partition >> $slurmf
 echo \#SBATCH -J $job_name >> $slurmf
 echo \#SBATCH -o $runout >> $slurmf
-if [[ "$partition" == scavenge ]]
-then
-    echo \#SBATCH --requeue >> $slurmf
-fi
 echo sed -n \"\$\{SLURM_ARRAY_TASK_ID\}p\" "$taskf" \| /bin/bash >> $slurmf
 cat $slurmf
 
